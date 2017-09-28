116b9f6a2cdb3ec74aa8d86390bbb152dedaf462
Sami <sampatius@hotmail.com>
2017-09-28 10:42:22 +0300

    Created GCodeParser class
    Initial creation

---
+++ inc/GCodeParser.h
@@ -0,0 +1,17 @@
+/*
+ * GCodeParser.h
+ *
+ *  Created on: Sep 28, 2017
+ *      Author: Sami
+ */
+
+#ifndef GCODEPARSER_H_
+#define GCODEPARSER_H_
+
+class GCodeParser {
+public:
+	GCodeParser();
+	virtual ~GCodeParser();
+};
+
+#endif /* GCODEPARSER_H_ */
---
+++ src/GCodeParser.cpp
@@ -0,0 +1,18 @@
+/*
+ * GCodeParser.cpp
+ *
+ *  Created on: Sep 28, 2017
+ *      Author: Sami
+ */
+
+#include "GCodeParser.h"
+
+GCodeParser::GCodeParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GCodeParser::~GCodeParser() {
+	// TODO Auto-generated destructor stub
+}
+
