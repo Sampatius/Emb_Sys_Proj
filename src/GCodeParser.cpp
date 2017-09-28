73d3bd7f5409d9192a0eb4f06be5c3321a8bc4b5
Sami <sampatius@hotmail.com>
2017-09-28 11:40:26 +0300

    Working on parsing options

--- src/GCodeParser.cpp
+++ src/GCodeParser.cpp
@@ -23,16 +23,16 @@ void GCodeParser::read() {
 
 	len = USB_receive((uint8_t *) str, 96);
 	if (len > 0) {
-		for (int i = 0; i < len; ++i) {
+		for (uint32_t i = 0; i < len; ++i) {
 			buffStr[i] = str[i];
 		}
-		if (buffStr[0] == 'M' && buffStr[1] == '1' && buffStr[2] == ' ') {
-			USB_send((uint8_t *) okResponse, strlen(okResponse));
-		} else if (buffStr[0] == 'M' && buffStr[1] == '1'
-				&& buffStr[2] == '0') {
+		if (strncmp(buffStr, "M10", 4)) {
 			USB_send((uint8_t *) mkInit, strlen(mkInit));
 			USB_send((uint8_t *) okResponse, strlen(okResponse));
-		} else if (buffStr[0] == 'G' && buffStr[1] == '1') {
+		} else if (strncmp(buffStr, "M1", 3)) {
+			USB_send((uint8_t *) okResponse, strlen(okResponse));
+
+		} else if (strncmp(buffStr, "G1", 3)) {
 			USB_send((uint8_t *) okResponse, strlen(okResponse));
 		}
 	}
