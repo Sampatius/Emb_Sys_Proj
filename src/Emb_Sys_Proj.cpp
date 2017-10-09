48a528ef391042e75f972842acdfcaee7024403c
Danituss <dan.huiko14@gmail.com>
2017-10-09 11:02:55 +0300

    Servo implementation
    Functionality in parser NOT implemented.

--- src/Emb_Sys_Proj.cpp
+++ src/Emb_Sys_Proj.cpp
@@ -136,7 +136,22 @@ void vConfigureTimerForRunTimeStats(void) {
 	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
 	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
 }
+
+void SCTLARGE0_Init(void) {
+	LPC_SCTLARGE0->CONFIG |= (1 << 17); // two 16-bit timers, auto limit
+	LPC_SCTLARGE0->CTRL_L |= (12-1) << 5; // set prescaler, SCTimer/PWM clock = 6 MHz
+
+	LPC_SCTLARGE0->MATCHREL[0].L = 120000-1; // match 0 @ 120000/6MHz = 10 usec (100 kHz PWM freq)
+	LPC_SCTLARGE0->MATCHREL[1].L = 9000; // match 1 used for duty cycle (in 10 steps)
+	LPC_SCTLARGE0->EVENT[0].STATE = 0xFFFFFFFF; // event 0 happens in all states
+	LPC_SCTLARGE0->EVENT[0].CTRL = (1 << 12); // match 0 condition only
+	LPC_SCTLARGE0->EVENT[1].STATE = 0xFFFFFFFF; // event 1 happens in all states
+	LPC_SCTLARGE0->EVENT[1].CTRL = (1 << 0) | (1 << 12); // match 1 condition only
+	LPC_SCTLARGE0->OUT[0].SET = (1 << 0); // event 0 will set SCTx_OUT0
+	LPC_SCTLARGE0->OUT[0].CLR = (1 << 1); // event 1 will clear SCTx_OUT0
+	LPC_SCTLARGE0->CTRL_L &= ~(1 << 2);
 }
+} // End of extern "C"
 
 /* TASKS */
 
@@ -273,6 +288,11 @@ int main(void) {
 	commandQueue = xQueueCreate(10, sizeof(coordObject));
 
 	Chip_RIT_Init(LPC_RITIMER);
+
+	Chip_SWM_MovablePortPinAssign(SWM_SCT0_OUT0_O, 0, 10);
+	Chip_SCT_Init(LPC_SCTLARGE0);
+	SCTLARGE0_Init();
+
 
 	NVIC_SetPriority(RITIMER_IRQn,
 	configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1);
